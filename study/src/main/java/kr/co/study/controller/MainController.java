c9906332bb3ea1dff6522519f1d7adf568dbda5d
oharam0413 <oharam0413@gmail.com>
2024-03-29 16:36:26 +0900

    sboard

---
+++ study/src/main/java/kr/co/study/controller/MainController.java
@@ -0,0 +1,22 @@
+package kr.co.study.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("hello")
+    public String Hello(Model model){
+
+        model.addAttribute("data","hello!!!");
+
+        return "hello";
+
+    }
+
+
+
+}
